29fb8a8f4f04ca27653e6625d57f3e8b90df09b3
Chris <chris.finlow@gmail.com>
2023-11-19 09:47:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# AOC 2023
+
+This project was inspired by `https://github.com/SpexGuy/Zig-AoC-Template`
+
+I wanted to use their repo as a template, but I couldn't get it to work with my Zig version.
+
+So, I had a go at creating my own version (but copying their generate code!) and it seems to have worked :D
+
+This year I am using Zig to try and learn the language.
---
+++ build.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    var day: u32 = 1;
+    while (day <= 25) : (day += 1) {
+        const dayString = b.fmt("day{:0>2}", .{day});
+        const zigFile = b.fmt("src/{s}.zig", .{dayString});
+        const runDayString = b.fmt("run-{s}", .{dayString});
+        const testDayString = b.fmt("test-{s}", .{dayString});
+        const exe = b.addExecutable(.{
+            .name = dayString,
+            // In this case the main source file is merely a path, however, in more
+            // complicated build scripts, this could be a generated file.
+            .root_source_file = .{ .path = zigFile },
+            .target = target,
+            .optimize = optimize,
+        });
+
+        // This declares intent for the executable to be installed into the
+        // standard location when the user invokes the "install" step (the default
+        // step when running `zig build`).
+        b.installArtifact(exe);
+
+        // This *creates* a Run step in the build graph, to be executed when another
+        // step is evaluated that depends on it. The next line below will establish
+        // such a dependency.
+        const run_cmd = b.addRunArtifact(exe);
+
+        // By making the run step depend on the install step, it will be run from the
+        // installation directory rather than directly from within the cache directory.
+        // This is not necessary, however, if the application depends on other installed
+        // files, this ensures they will be present and in the expected location.
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        // This allows the user to pass arguments to the application in the build
+        // command itself, like this: `zig build run -- arg1 arg2 etc`
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        // This creates a build step. It will be visible in the `zig build --help` menu,
+        // and can be selected like this: `zig build run`
+        // This will evaluate the `run` step rather than the default, which is "install".
+        const run_step = b.step(runDayString, b.fmt("Run {s}", .{dayString}));
+        run_step.dependOn(&run_cmd.step);
+
+        // Creates a step for unit testing. This only builds the test executable
+        // but does not run it.
+        const unit_tests = b.addTest(.{
+            .root_source_file = .{ .path = zigFile },
+            .target = target,
+            .optimize = optimize,
+        });
+
+        const run_unit_tests = b.addRunArtifact(unit_tests);
+
+        // Similar to creating the run step earlier, this exposes a `test` step to
+        // the `zig build --help` menu, providing a way for the user to request
+        // running the unit tests.
+        const test_step = b.step(testDayString, "Run unit tests");
+        test_step.dependOn(&run_unit_tests.step);
+    }
+
+    {
+        const exe = b.addExecutable(.{
+            .name = "generate",
+            // In this case the main source file is merely a path, however, in more
+            // complicated build scripts, this could be a generated file.
+            .root_source_file = .{ .path = "./template/generate.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
+
+        // This declares intent for the executable to be installed into the
+        // standard location when the user invokes the "install" step (the default
+        // step when running `zig build`).
+        b.installArtifact(exe);
+
+        // This *creates* a Run step in the build graph, to be executed when another
+        // step is evaluated that depends on it. The next line below will establish
+        // such a dependency.
+        const run_cmd = b.addRunArtifact(exe);
+
+        // By making the run step depend on the install step, it will be run from the
+        // installation directory rather than directly from within the cache directory.
+        // This is not necessary, however, if the application depends on other installed
+        // files, this ensures they will be present and in the expected location.
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        // This allows the user to pass arguments to the application in the build
+        // command itself, like this: `zig build run -- arg1 arg2 etc`
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        // This creates a build step. It will be visible in the `zig build --help` menu,
+        // and can be selected like this: `zig build run`
+        // This will evaluate the `run` step rather than the default, which is "install".
+        const run_step = b.step("generate", "Generate templates");
+        run_step.dependOn(&run_cmd.step);
+    }
+}
