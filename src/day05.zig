c85280cb95393a9b115dc99b1ebc9a6654336003
Chris <41023695+Christheoreo@users.noreply.github.com>
2023-12-06 20:23:27 +0000

    Day05 & Day06 :white_check_mark:
    * added template and data
    
    * day 05 prep
    
    * Making some progress where possible
    
    * part 1 works
    
    * part 2 code
    
    * cleaned code
    
    * finished day6part 1
    
    * completed day06
    
    * Added tests
    
    * Cleaned day 06

--- src/day05.zig
+++ src/day05.zig
@@ -9,8 +9,488 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day05.txt");
+const testData = @embedFile("data/day05.test.txt");
 
-pub fn main() !void {}
+const MapX = struct { destinationRangeStart: u64, sourceRangeStart: u64, rangeLength: u64 };
+
+pub fn main() !void {
+    var startTimePart = std.time.nanoTimestamp();
+    var partOneAnswer = try solvePartOne(data);
+    // var partOneAnswer = try solvePartOne(testData);
+    var elapsedTimePart: i128 = std.time.nanoTimestamp() - startTimePart;
+    const oneMil: f128 = 1_000_000;
+    var floatingPoint: f128 = @floatFromInt(elapsedTimePart);
+    var mili: f128 = floatingPoint / oneMil;
+    std.debug.print("Part 1 took: {} nanoseconds and {} miliseconds\n", .{ elapsedTimePart, mili });
+    std.debug.print("Answer to part one is {}\n", .{partOneAnswer});
+
+    startTimePart = std.time.nanoTimestamp();
+    const partTwoAnswer = try solvePartTwo(data);
+    const idk: f128 = 1_000_000_000;
+    elapsedTimePart = std.time.nanoTimestamp() - startTimePart;
+    mili = floatingPoint / idk;
+    floatingPoint = @floatFromInt(elapsedTimePart);
+    mili = floatingPoint / oneMil;
+    std.debug.print("Part 2 took: {} seconds \n", .{mili});
+    std.debug.print("Answer to part two is {}\n", .{partTwoAnswer});
+}
+
+fn solvePartOne(buffer: []const u8) !u64 {
+    const allocator = std.heap.page_allocator;
+    var lines = std.mem.splitAny(u8, buffer, "\n");
+    var seedToSoilMaps = std.ArrayList(MapX).init(allocator);
+    var soilToFertMaps = std.ArrayList(MapX).init(allocator);
+    var fertToWaterMaps = std.ArrayList(MapX).init(allocator);
+    var waterToLightMaps = std.ArrayList(MapX).init(allocator);
+    var lightToTempMaps = std.ArrayList(MapX).init(allocator);
+    var tempToHumidMaps = std.ArrayList(MapX).init(allocator);
+    var humidToLocationMaps = std.ArrayList(MapX).init(allocator);
+    defer seedToSoilMaps.deinit();
+    // TODO defer the rest
+    var lineIndex: usize = 0;
+    var activeMapperIndex: u32 = 0;
+    var seeds = std.ArrayList(u64).init(allocator);
+    var answer: u64 = 0;
+    while (lines.next()) |line| {
+        defer lineIndex += 1;
+        if (line.len == 0) continue;
+        const seedToSoil = "seed-to-soil map:";
+        const soilToFert = "soil-to-fertilizer map:";
+        const fertToWater = "fertilizer-to-water map:";
+        const waterToLight = "water-to-light map:";
+        const lightToTemp = "light-to-temperature map:";
+        const tempToHumid = "temperature-to-humidity map:";
+        const humidToLocation = "humidity-to-location map:";
+
+        if (lineIndex == 0) {
+            // the seeds
+            var y = line[7..];
+            var yIterator = std.mem.splitAny(u8, y, " ");
+            while (yIterator.next()) |yy| {
+                try seeds.append(try parseInt(u64, yy, 10));
+            }
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, seedToSoil)) {
+            activeMapperIndex = 0;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, soilToFert)) {
+            activeMapperIndex = 1;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, fertToWater)) {
+            activeMapperIndex = 2;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, waterToLight)) {
+            activeMapperIndex = 3;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, lightToTemp)) {
+            activeMapperIndex = 4;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, tempToHumid)) {
+            activeMapperIndex = 5;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, humidToLocation)) {
+            activeMapperIndex = 6;
+            continue;
+        }
+
+        // Should have a line with 3 numbers split by spaces
+
+        var numbersAsBytesIterator = std.mem.splitAny(u8, line, " ");
+        var m = MapX{ .destinationRangeStart = 0, .sourceRangeStart = 0, .rangeLength = 0 };
+        var numbersAsBytesIteratorIndex: usize = 0;
+        while (numbersAsBytesIterator.next()) |xx| {
+            defer numbersAsBytesIteratorIndex += 1;
+            var number = try parseInt(u64, xx, 10);
+            switch (numbersAsBytesIteratorIndex) {
+                0 => {
+                    m.destinationRangeStart = number;
+                },
+                1 => {
+                    m.sourceRangeStart = number;
+                },
+                else => {
+                    m.rangeLength = number;
+                },
+            }
+        }
+
+        switch (activeMapperIndex) {
+            0 => {
+                try seedToSoilMaps.append(m);
+            },
+            1 => {
+                try soilToFertMaps.append(m);
+            },
+            2 => {
+                try fertToWaterMaps.append(m);
+            },
+            3 => {
+                try waterToLightMaps.append(m);
+            },
+            4 => {
+                try lightToTempMaps.append(m);
+            },
+            5 => {
+                try tempToHumidMaps.append(m);
+            },
+            6 => {
+                try humidToLocationMaps.append(m);
+            },
+            else => {
+                //
+            },
+        }
+        // std.debug.print("Line = {s} ({})\n", .{ line, line.len });
+    }
+
+    for (seeds.items) |seed| {
+        // Start with seed to soil
+        // is it withiin source range and destination range? if so, add the range.
+        // conitnue here.
+        var destValue: u64 = seed;
+        for (seedToSoilMaps.items) |seedToSoilMap| {
+            const mn: u64 = seedToSoilMap.sourceRangeStart;
+            const mx: u64 = mn + seedToSoilMap.rangeLength;
+            if (destValue >= mn and destValue <= mx) {
+                destValue = (destValue - mn) + seedToSoilMap.destinationRangeStart;
+                break;
+            }
+        }
+
+        // std.debug.print("After seed-to-soil value is {}\n", .{destValue});
+
+        for (soilToFertMaps.items) |soilToFertMap| {
+            const mn: u64 = soilToFertMap.sourceRangeStart;
+            const mx: u64 = mn + soilToFertMap.rangeLength;
+            if (destValue >= mn and destValue <= mx) {
+                destValue = (destValue - mn) + soilToFertMap.destinationRangeStart;
+                break;
+            }
+        }
+        // std.debug.print("After soil-to-fert value is {}\n", .{destValue});
+
+        for (fertToWaterMaps.items) |fertoWaterMap| {
+            const mn: u64 = fertoWaterMap.sourceRangeStart;
+            const mx: u64 = mn + fertoWaterMap.rangeLength;
+            if (destValue >= mn and destValue <= mx) {
+                destValue = (destValue - mn) + fertoWaterMap.destinationRangeStart;
+                break;
+            }
+        }
+
+        // std.debug.print("After fert-to-water value is {}\n", .{destValue});
+
+        for (waterToLightMaps.items) |waterToLightMap| {
+            const mn: u64 = waterToLightMap.sourceRangeStart;
+            const mx: u64 = mn + waterToLightMap.rangeLength;
+            if (destValue >= mn and destValue <= mx) {
+                destValue = (destValue - mn) + waterToLightMap.destinationRangeStart;
+                break;
+            }
+        }
+        // std.debug.print("After water-to-light value is {}\n", .{destValue});
+
+        for (lightToTempMaps.items) |genericMap| {
+            const mn: u64 = genericMap.sourceRangeStart;
+            const mx: u64 = mn + genericMap.rangeLength;
+            if (destValue >= mn and destValue <= mx) {
+                destValue = (destValue - mn) + genericMap.destinationRangeStart;
+                break;
+            }
+        }
+
+        // std.debug.print("After light-to-temp value is {}\n", .{destValue});
+
+        for (tempToHumidMaps.items) |genericMap| {
+            const mn: u64 = genericMap.sourceRangeStart;
+            const mx: u64 = mn + genericMap.rangeLength;
+            if (destValue >= mn and destValue <= mx) {
+                destValue = (destValue - mn) + genericMap.destinationRangeStart;
+                break;
+            }
+        }
+
+        // std.debug.print("After temp-to-humid value is {}\n", .{destValue});
+
+        for (humidToLocationMaps.items) |genericMap| {
+            const mn: u64 = genericMap.sourceRangeStart;
+            const mx: u64 = mn + genericMap.rangeLength;
+            if (destValue >= mn and destValue <= mx) {
+                destValue = (destValue - mn) + genericMap.destinationRangeStart;
+                break;
+            }
+        }
+
+        // std.debug.print("After humid-to-location value is {}\n\n\n\n", .{destValue});
+
+        switch (answer) {
+            0 => {
+                answer = destValue;
+            },
+            else => {
+                if (answer > destValue) {
+                    answer = destValue;
+                }
+            },
+        }
+    }
+
+    // const seedToSoilMaps = allocator.alloc(comptime T: type, n: usize)
+    return answer;
+}
+
+fn solvePartTwo(buffer: []const u8) !u64 {
+    const allocator = std.heap.page_allocator;
+    var lines = std.mem.splitAny(u8, buffer, "\n");
+    var seedToSoilMaps = std.ArrayList(MapX).init(allocator);
+    var soilToFertMaps = std.ArrayList(MapX).init(allocator);
+    var fertToWaterMaps = std.ArrayList(MapX).init(allocator);
+    var waterToLightMaps = std.ArrayList(MapX).init(allocator);
+    var lightToTempMaps = std.ArrayList(MapX).init(allocator);
+    var tempToHumidMaps = std.ArrayList(MapX).init(allocator);
+    var humidToLocationMaps = std.ArrayList(MapX).init(allocator);
+    defer seedToSoilMaps.deinit();
+    // TODO defer the rest
+    var lineIndex: usize = 0;
+    var activeMapperIndex: u32 = 0;
+    var seeds = std.ArrayList(@Vector(2, u64)).init(allocator);
+    var answer: u64 = 0;
+
+    var totalIterations: u128 = 0;
+    while (lines.next()) |line| {
+        defer lineIndex += 1;
+        if (line.len == 0) continue;
+        const seedToSoil = "seed-to-soil map:";
+        const soilToFert = "soil-to-fertilizer map:";
+        const fertToWater = "fertilizer-to-water map:";
+        const waterToLight = "water-to-light map:";
+        const lightToTemp = "light-to-temperature map:";
+        const tempToHumid = "temperature-to-humidity map:";
+        const humidToLocation = "humidity-to-location map:";
+
+        if (lineIndex == 0) {
+            // the seeds
+            var y = line[7..];
+            var yIndex: usize = 0;
+            var yIterator = std.mem.splitAny(u8, y, " ");
+            var vec: @Vector(2, u64) = .{ 0, 0 };
+            while (yIterator.next()) |yy| {
+                defer yIndex += 1;
+                if (yIndex % 2 == 0) {
+                    vec[0] = try parseInt(u64, yy, 10);
+                    continue;
+                }
+                vec[1] = try parseInt(u64, yy, 10);
+                totalIterations += (vec[0] + vec[1]);
+                try seeds.append(vec);
+            }
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, seedToSoil)) {
+            activeMapperIndex = 0;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, soilToFert)) {
+            activeMapperIndex = 1;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, fertToWater)) {
+            activeMapperIndex = 2;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, waterToLight)) {
+            activeMapperIndex = 3;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, lightToTemp)) {
+            activeMapperIndex = 4;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, tempToHumid)) {
+            activeMapperIndex = 5;
+            continue;
+        }
+
+        if (std.mem.eql(u8, line, humidToLocation)) {
+            activeMapperIndex = 6;
+            continue;
+        }
+
+        // Should have a line with 3 numbers split by spaces
+
+        var numbersAsBytesIterator = std.mem.splitAny(u8, line, " ");
+        var m = MapX{ .destinationRangeStart = 0, .sourceRangeStart = 0, .rangeLength = 0 };
+        var numbersAsBytesIteratorIndex: usize = 0;
+        while (numbersAsBytesIterator.next()) |xx| {
+            defer numbersAsBytesIteratorIndex += 1;
+            var number = try parseInt(u64, xx, 10);
+            switch (numbersAsBytesIteratorIndex) {
+                0 => {
+                    m.destinationRangeStart = number;
+                },
+                1 => {
+                    m.sourceRangeStart = number;
+                },
+                else => {
+                    m.rangeLength = number;
+                },
+            }
+        }
+
+        switch (activeMapperIndex) {
+            0 => {
+                try seedToSoilMaps.append(m);
+            },
+            1 => {
+                try soilToFertMaps.append(m);
+            },
+            2 => {
+                try fertToWaterMaps.append(m);
+            },
+            3 => {
+                try waterToLightMaps.append(m);
+            },
+            4 => {
+                try lightToTempMaps.append(m);
+            },
+            5 => {
+                try tempToHumidMaps.append(m);
+            },
+            6 => {
+                try humidToLocationMaps.append(m);
+            },
+            else => {
+                //
+            },
+        }
+        // std.debug.print("Line = {s} ({})\n", .{ line, line.len });
+    }
+    for (seeds.items) |seed| {
+        var minValue = seed[0];
+
+        const maxBoundary = seed[0] + seed[1];
+
+        while (minValue < maxBoundary) : (minValue += 1) {
+            var destValue: u64 = minValue;
+            // std.debug.print("Starting value is {}\n", .{destValue});
+            for (seedToSoilMaps.items) |seedToSoilMap| {
+                const mn: u64 = seedToSoilMap.sourceRangeStart;
+                const mx: u64 = mn + seedToSoilMap.rangeLength;
+                if (destValue >= mn and destValue <= mx) {
+                    destValue = (destValue - mn) + seedToSoilMap.destinationRangeStart;
+                    break;
+                }
+            }
+
+            // std.debug.print("After seed-to-soil value is {}\n", .{destValue});
+
+            for (soilToFertMaps.items) |soilToFertMap| {
+                const mn: u64 = soilToFertMap.sourceRangeStart;
+                const mx: u64 = mn + soilToFertMap.rangeLength;
+                if (destValue >= mn and destValue <= mx) {
+                    destValue = (destValue - mn) + soilToFertMap.destinationRangeStart;
+                    break;
+                }
+            }
+            // std.debug.print("After soil-to-fert value is {}\n", .{destValue});
+
+            for (fertToWaterMaps.items) |fertoWaterMap| {
+                const mn: u64 = fertoWaterMap.sourceRangeStart;
+                const mx: u64 = mn + fertoWaterMap.rangeLength;
+                if (destValue >= mn and destValue <= mx) {
+                    destValue = (destValue - mn) + fertoWaterMap.destinationRangeStart;
+                    break;
+                }
+            }
+
+            // std.debug.print("After fert-to-water value is {}\n", .{destValue});
+
+            for (waterToLightMaps.items) |waterToLightMap| {
+                const mn: u64 = waterToLightMap.sourceRangeStart;
+                const mx: u64 = mn + waterToLightMap.rangeLength;
+                if (destValue >= mn and destValue <= mx) {
+                    destValue = (destValue - mn) + waterToLightMap.destinationRangeStart;
+                    break;
+                }
+            }
+            // std.debug.print("After water-to-light value is {}\n", .{destValue});
+
+            for (lightToTempMaps.items) |genericMap| {
+                const mn: u64 = genericMap.sourceRangeStart;
+                const mx: u64 = mn + genericMap.rangeLength;
+                if (destValue >= mn and destValue <= mx) {
+                    destValue = (destValue - mn) + genericMap.destinationRangeStart;
+                    break;
+                }
+            }
+
+            // std.debug.print("After light-to-temp value is {}\n", .{destValue});
+
+            for (tempToHumidMaps.items) |genericMap| {
+                const mn: u64 = genericMap.sourceRangeStart;
+                const mx: u64 = mn + genericMap.rangeLength;
+                if (destValue >= mn and destValue <= mx) {
+                    destValue = (destValue - mn) + genericMap.destinationRangeStart;
+                    break;
+                }
+            }
+
+            // std.debug.print("After temp-to-humid value is {}\n", .{destValue});
+
+            for (humidToLocationMaps.items) |genericMap| {
+                const mn: u64 = genericMap.sourceRangeStart;
+                const mx: u64 = mn + genericMap.rangeLength;
+                if (destValue >= mn and destValue <= mx) {
+                    destValue = (destValue - mn) + genericMap.destinationRangeStart;
+                    break;
+                }
+            }
+
+            // std.debug.print("After humid-to-location value is {}\n\n\n\n", .{destValue});
+
+            switch (answer) {
+                0 => {
+                    answer = destValue;
+                },
+                else => {
+                    if (answer > destValue) {
+                        answer = destValue;
+                    }
+                },
+            }
+        }
+    }
+    // const seedToSoilMaps = allocator.alloc(comptime T: type, n: usize)
+    return answer;
+}
+
+test "part one should solve" {
+    try std.testing.expect(try solvePartOne(testData) == 35);
+}
+
+test "part two should solve" {
+    try std.testing.expect(try solvePartTwo(testData) == 46);
+}
 
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
--- src/day06.zig
+++ src/day06.zig
@@ -10,7 +10,73 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day06.txt");
 
-pub fn main() !void {}
+pub fn main() !void {
+    var startTimePart = std.time.nanoTimestamp();
+    const times = [_]u32{ 50, 74, 86, 85 };
+    const distances = [_]u32{ 242, 1017, 1691, 1252 };
+    var partOneAnswer = try solvePartOne(&times, &distances);
+    var elapsedTimePart: i128 = std.time.nanoTimestamp() - startTimePart;
+    const oneMil: f128 = 1_000_000;
+    var floatingPoint: f128 = @floatFromInt(elapsedTimePart);
+    var mili: f128 = floatingPoint / oneMil;
+    std.debug.print("Part 1 took: {} nanoseconds and {} miliseconds\n", .{ elapsedTimePart, mili });
+    std.debug.print("Answer to part one is {}\n", .{partOneAnswer});
+
+    startTimePart = std.time.nanoTimestamp();
+    const partTwoAnswer = try solvePartTwo(50748685, 242101716911252);
+    elapsedTimePart = std.time.nanoTimestamp() - startTimePart;
+
+    floatingPoint = @floatFromInt(elapsedTimePart);
+    mili = floatingPoint / oneMil;
+    std.debug.print("Part 2 took: {} nanoseconds and {} miliseconds\n", .{ elapsedTimePart, mili });
+    std.debug.print("Answer to part two is {}\n", .{partTwoAnswer});
+}
+
+fn solvePartOne(times: []const u32, distances: []const u32) !u64 {
+    var answer: u32 = 1;
+
+    for (times, 0..) |time, i| {
+        var totalWays: u32 = 0;
+        const distanceToBeat = distances[i];
+        var miliSecondHold: u32 = 1;
+        while (miliSecondHold < time) : (miliSecondHold += 1) {
+            const travelTime = time - miliSecondHold;
+            const distanceTravelled = travelTime * miliSecondHold;
+            if (distanceTravelled > distanceToBeat) {
+                totalWays += 1;
+            }
+        }
+        answer *= totalWays;
+    }
+
+    return answer;
+}
+
+fn solvePartTwo(time: u64, distanceToBeat: u64) !u64 {
+    var answer: u32 = 0;
+    var miliSecondHold: u32 = 1;
+    while (miliSecondHold < time) : (miliSecondHold += 1) {
+        const travelTime = time - miliSecondHold;
+        const distanceTravelled = travelTime * miliSecondHold;
+        if (distanceTravelled > distanceToBeat) {
+            answer += 1;
+        }
+    }
+
+    return answer;
+}
+
+test "Part One should solve" {
+    const times = [_]u32{ 7, 15, 30 };
+    const distances = [_]u32{ 9, 40, 200 };
+    try std.testing.expect(try solvePartOne(&times, &distances) == 288);
+}
+
+test "Part two should solve" {
+    const time: u32 = 71530;
+    const distanceToBeat = 940200;
+    try std.testing.expect(try solvePartTwo(time, distanceToBeat) == 71503);
+}
 
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
