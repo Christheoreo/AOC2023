5b14aea70b1c64db02c79252529de18b62290638
Chris <chris.finlow@gmail.com>
2023-12-08 21:07:52 +0000

    Set base

--- src/day08.zig
+++ src/day08.zig
@@ -9,8 +9,44 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day08.txt");
+const testData = @embedFile("data/day07.test.txt");
+pub fn main() !void {
+    var startTimePart = std.time.nanoTimestamp();
+    var partOneAnswer = try solvePartOne(data);
+    var elapsedTimePart: i128 = std.time.nanoTimestamp() - startTimePart;
+    const oneMil: f128 = 1_000_000;
+    var floatingPoint: f128 = @floatFromInt(elapsedTimePart);
+    var mili: f128 = floatingPoint / oneMil;
+    std.debug.print("Part 1 took: {} nanoseconds and {} miliseconds\n", .{ elapsedTimePart, mili });
+    std.debug.print("Answer to part one is {}\n", .{partOneAnswer});
 
-pub fn main() !void {}
+    startTimePart = std.time.nanoTimestamp();
+    const partTwoAnswer = try solvePartTwo(data);
+    elapsedTimePart = std.time.nanoTimestamp() - startTimePart;
+
+    floatingPoint = @floatFromInt(elapsedTimePart);
+    mili = floatingPoint / oneMil;
+    std.debug.print("Part 2 took: {} nanoseconds and {} miliseconds\n", .{ elapsedTimePart, mili });
+    std.debug.print("Answer to part two is {}\n", .{partTwoAnswer});
+}
+
+pub fn solvePartOne(buffer: []const u8) !u32 {
+    _ = buffer;
+    return 0;
+}
+
+pub fn solvePartTwo(buffer: []const u8) !u32 {
+    _ = buffer;
+    return 0;
+}
+
+test "part one should solve" {
+    try std.testing.expect(try solvePartOne(testData) == 2);
+}
+
+test "part two should solve" {
+    try std.testing.expect(try solvePartTwo(testData) == 2);
+}
 
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
