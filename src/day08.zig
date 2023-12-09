4768e7534cfbdaa2a5811a9e15630a049dac19b4
Chris <chris.finlow@gmail.com>
2023-12-09 11:39:04 +0000

    part 1 solved

--- src/day08.zig
+++ src/day08.zig
@@ -9,7 +9,10 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day08.txt");
-const testData = @embedFile("data/day07.test.txt");
+const testData = @embedFile("data/day08.test.txt");
+const testData2 = @embedFile("data/day08.test2.txt");
+
+const MapDetail = struct { location: [3]u8, left: [3]u8, right: [3]u8 };
 pub fn main() !void {
     var startTimePart = std.time.nanoTimestamp();
     var partOneAnswer = try solvePartOne(data);
@@ -31,8 +34,56 @@ pub fn main() !void {
 }
 
 pub fn solvePartOne(buffer: []const u8) !u32 {
-    _ = buffer;
-    return 0;
+    var answer: u32 = 0;
+    var lines = std.mem.splitAny(u8, buffer, "\n");
+    var lineIndex: usize = 0;
+    var allocator = std.heap.page_allocator;
+    var instructions = std.ArrayList(u8).init(allocator);
+    defer instructions.deinit();
+    var maps = std.ArrayList(MapDetail).init(allocator);
+    defer maps.deinit();
+
+    var detailsMap = std.AutoHashMap([3]u8, MapDetail).init(allocator);
+    defer detailsMap.deinit();
+
+    while (lines.next()) |line| {
+        defer lineIndex += 1;
+        if (lineIndex == 0) {
+            var i: usize = 0;
+            while (i < line.len) : (i += 1) {
+                try instructions.append(line[i]);
+            }
+            continue;
+        }
+        if (lineIndex == 1) continue;
+        const detail = MapDetail{ .location = [3]u8{ line[0], line[1], line[2] }, .left = [3]u8{ line[7], line[8], line[9] }, .right = [3]u8{ line[12], line[13], line[14] } };
+
+        try detailsMap.put([3]u8{ line[0], line[1], line[2] }, detail);
+        try maps.append(detail);
+    }
+
+    const instructionsArray = instructions.items;
+    var instructionIndex: u32 = 0;
+
+    var currentDetail: MapDetail = detailsMap.get([3]u8{ 'A', 'A', 'A' }).?;
+    while (!std.mem.eql(u8, &currentDetail.location, &[3]u8{ 'Z', 'Z', 'Z' })) {
+        defer answer += 1;
+        if (instructionsArray[instructionIndex] == 'L') {
+            currentDetail = detailsMap.get(currentDetail.left).?;
+        } else {
+            currentDetail = detailsMap.get(currentDetail.right).?;
+        }
+        if (instructionIndex + 1 >= instructionsArray.len) {
+            instructionIndex = 0;
+        } else instructionIndex += 1;
+    }
+    // for (maps.items) |detail| {
+    //     std.debug.print("Detail location = .{s}. left = .{s}. right = .{s}.\n", .{ detail.location, detail.left, detail.right });
+    //     if (instructionIndex >= instructionsArray.len) {
+    //         instructionIndex = 0;
+    //     } else instructionIndex += 1;
+    // }
+    return answer;
 }
 
 pub fn solvePartTwo(buffer: []const u8) !u32 {
